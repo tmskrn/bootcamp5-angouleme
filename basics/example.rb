02ece79de030f8933934e0ed88d815d85ab0ec5a
Magda Gumularz <mmgumularz@gmail.com>
2014-11-03 11:36:11 +0100

    #5 spaces between variables in intializer

--- basics/example.rb
+++ basics/example.rb
@@ -2,7 +2,7 @@ class Article
  attr_accessor :likes, :dislikes
  attr_reader :title, :body, :author, :created_at
 
- def initialize(title,body,author = nil)
+ def initialize(title, body, author = nil)
   @title, @body, @author = title, body, author
   @created_at = Time.now
   @likes = 0
