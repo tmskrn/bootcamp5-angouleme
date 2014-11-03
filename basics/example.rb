8da791bfb6242128ced2719ef18383f013b189ac
Magda Gumularz <mmgumularz@gmail.com>
2014-11-03 11:00:09 +0100

    #4 Article class created

---
+++ basics/example.rb
@@ -0,0 +1,38 @@
+class Article
+
+ def initialize(title,body,author)
+  @title, @body, @author = title, body, author
+ end
+
+ def title
+  @title
+ end
+
+ def body
+  @body
+ end
+
+ def author
+  @author
+ end
+
+ def created_at
+  @created_at
+ end
+
+ def likes=(value)
+  @likes = value
+ end
+ 
+ def likes
+  @likes
+ end 
+
+ def dislikes=(value)
+  @dislikes = value
+ end
+
+ def dislikes
+  @dislikes
+ end
+end
