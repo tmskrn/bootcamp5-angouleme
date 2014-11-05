ba84397c1af657b170014bce74ed6d2df3d9262f
Magda Gumularz <mmgumularz@gmail.com>
2014-11-05 15:07:53 +0100

    #30 parking validations

--- app/models/parking.rb
+++ app/models/parking.rb
@@ -3,4 +3,9 @@ class Parking < ActiveRecord::Base
  belongs_to :owner, class_name: "Person"
  has_many :cars
  has_many :place_rents
+
+ validates_presence_of :places, :hour_price, :day_price
+ validates :kind, inclusion: {in: w%(outdoor indoor private or street)}
+ validates :hour_price, :day_price, numericality: true
+
 end
