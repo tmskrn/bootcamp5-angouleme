2ea1b97c310e98ecf49fcbc24f425da15ba9de64
Magda Gumularz <mmgumularz@gmail.com>
2014-11-05 15:44:15 +0100

    #31 fixed syntax

--- app/models/place_rent.rb
+++ app/models/place_rent.rb
@@ -2,5 +2,5 @@ class PlaceRent < ActiveRecord::Base
   belongs_to :car
   belongs_to :parking
 
-  validates_presence_of :start_date, :end_date, :parking, :car
+  validates :start_date, :end_date, :parking, :car, presence: true
 end
