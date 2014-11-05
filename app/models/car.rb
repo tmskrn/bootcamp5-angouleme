5ac88281e1fd1d76304753b76eb23575623f4951
Magda Gumularz <mmgumularz@gmail.com>
2014-11-05 15:35:14 +0100

    #28 fixed syntax

--- app/models/car.rb
+++ app/models/car.rb
@@ -2,5 +2,5 @@ class Car < ActiveRecord::Base
   belongs_to :owner, class_name: "Person"
   has_many :place_rents
 
-  validates_presence_of :owner, :model, :registration_number
+  validates :owner, :model, :registration_number, presence: true
 end
