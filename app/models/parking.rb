4f29b4a5dbbbdf1c4b73a324b87cbb05b7f9334d
Magda Gumularz <mmgumularz@gmail.com>
2014-11-05 15:28:49 +0100

    #30 newer syntax and indentation

--- app/models/parking.rb
+++ app/models/parking.rb
@@ -1,11 +1,11 @@
 class Parking < ActiveRecord::Base
- belongs_to :address
- belongs_to :owner, class_name: "Person"
- has_many :cars
- has_many :place_rents
+  belongs_to :address
+  belongs_to :owner, class_name: "Person"
+  has_many :cars
+  has_many :place_rents
 
- validates_presence_of :places, :hour_price, :day_price
- validates :kind, inclusion: {in: w%(outdoor indoor private or street)}
- validates :hour_price, :day_price, numericality: true
-
+  validates :places, presence: true
+  validates :hour_price, presence: true, numericality: true
+  validates :day_price, numericality: true
+  validates :kind, inclusion: {in: w%(outdoor indoor private or street)}
 end
