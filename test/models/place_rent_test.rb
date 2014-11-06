ff2da237c972cafd79e4927824bf2c1ebcdd7aad
Magda Gumularz <mmgumularz@gmail.com>
2014-11-06 13:56:11 +0100

    #31 tests for PlaceRent class validations

--- test/models/place_rent_test.rb
+++ test/models/place_rent_test.rb
@@ -1,7 +1,27 @@
 require 'test_helper'
 
 class PlaceRentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save place rent without start date" do
+   place_rent = PlaceRent.new(end_date: Time.now + 3.days, parking: parkings(:wroclaw), car: cars(:porsche))
+   assert place_rent.invalid?
+   assert_not place_rent.errors[:start_date].empty?
+  end
+
+  test "should not save place rent without end date" do
+   place_rent = PlaceRent.new(start_date: Time.now, parking: parkings(:wroclaw), car: cars(:porsche))
+   assert place_rent.invalid?
+   assert_not place_rent.errors[:end_date].empty?
+  end
+
+  test "should not save place rent without parking" do
+   place_rent = PlaceRent.new(start_date: Time.now, end_date: Time.now + 2.days, car: cars(:porsche))
+   assert place_rent.invalid?
+   assert_not place_rent.errors[:parking].empty?
+  end
+
+  test "should not save place rent without car" do
+   place_rent = PlaceRent.new(start_date: Time.now, end_date: Time.now + 2.days,  parking: parkings(:wroclaw))
+   assert place_rent.invalid?
+   assert_not place_rent.errors[:car].empty?
+  end
 end
