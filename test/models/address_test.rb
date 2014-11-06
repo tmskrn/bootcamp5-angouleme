00064af87d8b183f3e36f1e5aefc69ce3012d690
Magda Gumularz <mmgumularz@gmail.com>
2014-11-06 11:04:17 +0100

    #27 tests for Address validation

--- test/models/address_test.rb
+++ test/models/address_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class AddressTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+   test "should not save address without city" do
+    address = Address.new(street: "Zielona", zip_code: "32-440")
+    assert address.invalid?
+   end
+
+   test "should not save address without street" do
+    address = Address.new(city: "Sułkowice", zip_code: "32-440")
+    assert address.invalid?
+   end
+
+   test "should not save address without zip code" do
+    address = Address.new(city: "Sułkowice", street: "Zielona")
+    assert address.invalid?
+   end
+
+   test "should not save address when zip code has wrong format" do
+    address = Address.new(city: "Sułkowice", street: "Zielona", zip_code: "223")
+    assert address.invalid? 
+   end
 end
