1dec56dcd67d4d4d1acea06b32b5848f428ed02b
Magda Gumularz <mmgumularz@gmail.com>
2014-11-06 12:50:21 +0100

    #28 tests for Person class validations

--- app/models/parking.rb
+++ app/models/parking.rb
@@ -1,11 +1,11 @@
 class Parking < ActiveRecord::Base
   belongs_to :address
   belongs_to :owner, class_name: "Person"
-  has_many :cars
+  has_many :cars, through: :place_rents
   has_many :place_rents
 
   validates :places, presence: true
   validates :hour_price, presence: true, numericality: true
   validates :day_price,  presence: true, numericality: true
-  validates :kind, inclusion: {in: w%(outdoor indoor private or street)}
+  validates :kind, inclusion: {in: %w(outdoor indoor private or street)}
 end
