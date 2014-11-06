b8052325be7a19de49f35b043f24737a31f62d7d
Magda Gumularz <mmgumularz@gmail.com>
2014-11-06 15:08:02 +0100

    #29 test for Person class validations

--- test/models/person_test.rb
+++ test/models/person_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class PersonTest < ActiveSupport::TestCase
   test "should not save person without first name" do
-    person = Person.new(last_name: "nowak")
+    person = Person.new(last_name: "asdf")
     assert person.invalid?
     assert_not person.errors[:first_name].empty? 
   end
