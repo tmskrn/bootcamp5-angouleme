bbdce426c4c57a1eed3496eaf0d0fc8a53dd9480
Magda Gumularz <mmgumularz@gmail.com>
2014-11-06 11:43:06 +0100

    #17 moved assertions from setup method to test_initializer

--- basics/my_test.rb
+++ basics/my_test.rb
@@ -1,20 +1,20 @@
 require 'minitest/autorun'
-require './broken_example.rb'
-#require './article.rb'
+#require './broken_example.rb'
+require './article.rb'
 
 class ArticleTest < Minitest::Test
   def setup
    @article = Article.new('some title', 'some body', 'some author')
-   assert_equal 0, @article.likes
-   assert_equal 0, @article.dislikes
-   assert_equal 0, @article.points
-   assert_equal 0, @article.votes
   end
 
   def test_initialization
    assert_equal "some title", @article.title
    assert_equal "some body", @article.body
    assert_equal "some author", @article.author
+   assert_equal 0, @article.likes
+   assert_equal 0, @article.dislikes
+   assert_equal 0, @article.points
+   assert_equal 0, @article.votes
   end
 
   def test_initialization_with_anonymous_author
