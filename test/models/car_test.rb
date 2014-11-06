f40a268f23a3d170b50ebca659fe5ce59ec1a85a
Magda Gumularz <mmgumularz@gmail.com>
2014-11-06 12:40:11 +0100

    #28 tests for Car validations

--- test/models/car_test.rb
+++ test/models/car_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class CarTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save car without model" do
+    car = Car.new(registration_number: "1345", owner_id: 4)
+    assert car.invalid?
+    assert_not car.errors[:model].empty?
+  end
+
+  test "should not save car without registration number" do
+    car = Car.new(model: "bmw", owner_id: 4)
+    assert car.invalid?
+    assert_not car.errors[:registration_number].empty?
+  end
+
+  test "should not save car without owner" do
+    car = Car.new(registration_number: "1345", model: "bmw")
+    assert car.invalid?
+    assert_not car.errors[:owner].empty?
+  end
+
 end
