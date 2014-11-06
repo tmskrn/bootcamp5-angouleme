15239ca03d15414aa0f0fbc2ba4e8f056147e017
Magda Gumularz <mmgumularz@gmail.com>
2014-11-06 16:56:28 +0100

    #34 actions new and create in parkings_controller, form for creating new parking added

--- app/controllers/parkings_controller.rb
+++ app/controllers/parkings_controller.rb
@@ -7,5 +7,23 @@ class ParkingsController < ApplicationController
  def show
   @parking = Parking.find(params[:id])
  end
+
+ def new
+  @parking = Parking.new()
+ end
  
+ def create
+  @parking = Parking.new(parking_params)
+  
+  if @parking.save
+   redirect_to parking_path(@parking)
+  else
+   render 'new'
+  end 
+ end
+ 
+ private
+ def parking_params
+  params.require(:parking).permit(:kind, :hour_price, :day_price, :places, :owner_id, :address_id)
+ end
 end
