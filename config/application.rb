1a8f1e6c23fd6f28dc7e8d6de77b4b951431c8e3
Magda Gumularz <mmgumularz@gmail.com>
2014-11-10 20:19:17 +0100

    #44 utf8_sanitizier added

--- config/application.rb
+++ config/application.rb
@@ -1,13 +1,17 @@
 require File.expand_path('../boot', __FILE__)
 
+
 require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+#config.middleware.insert 0, Rack::UTF8Sanitizer
+
 module B
   class Application < Rails::Application
+  config.middleware.insert 0, Rack::UTF8Sanitizer
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
