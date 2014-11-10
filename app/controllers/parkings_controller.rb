693d5809ba001b757caa905fb2cfcb814159a350
Magda Gumularz <mmgumularz@gmail.com>
2014-11-10 16:56:03 +0100

    #33 fixed indentation

--- app/controllers/parkings_controller.rb
+++ app/controllers/parkings_controller.rb
@@ -1,51 +1,52 @@
 class ParkingsController < ApplicationController
 
- def index
-  @parkings = Parking.all
- end
+  def index
+    @parkings = Parking.all
+  end
 
- def show
-  @parking = Parking.find(params[:id])
- end
+  def show
+    @parking = Parking.find(params[:id])
+  end
 
- def new
-  @parking = Parking.new()
-  @parking.build_address
- end
+  def new
+    @parking = Parking.new
+    @parking.build_address
+  end
  
- def create
-  @parking = Parking.new(parking_params)
+  def create
+    @parking = Parking.new(parking_params)
   
-  if @parking.save
-   redirect_to @parking
-  else
-   render 'new'
-  end 
- end
+    if @parking.save
+      redirect_to @parking
+    else
+      render 'new'
+    end 
+  end
 
- def edit
-  @parking = Parking.find(params[:id]) 
-  @parking.build_address unless @parking.address.present?
- end
+  def edit
+    @parking = Parking.find(params[:id]) 
+    @parking.build_address unless @parking.address.present?
+  end
 
- def update
-  @parking = Parking.find(params[:id])
-  if @parking.update(parking_params)
-   redirect_to @parking
-  else
-   render 'edit'
-  end 
- end
+  def update
+    @parking = Parking.find(params[:id])
+    if @parking.update(parking_params)
+      redirect_to @parking
+    else
+      render 'edit'
+    end 
+  end
 
- def destroy
-  @parking = Parking.find(params[:id])
-  @parking.destroy
-  redirect_to parkings_url
- end
+  def destroy
+    @parking = Parking.find(params[:id])
+    @parking.destroy
+    redirect_to parkings_url
+  end
  
- private
- def parking_params
-  params.require(:parking).permit(:kind, :hour_price, :day_price, :places, 
+  private
+
+  def parking_params
+    params.require(:parking).permit(:kind, :hour_price, :day_price, :places, 
                                   :address_attributes => [:city, :street, :zip_code])
- end
+  end
 end
