fb97550c70031040f50371f8f559ad02872e1d3f
Magda Gumularz <mmgumularz@gmail.com>
2014-11-10 09:04:55 +0100

    #37 refactored current_controller_name method with test

--- app/helpers/controller_name_helper.rb
+++ app/helpers/controller_name_helper.rb
@@ -1,6 +1,5 @@
 module ControllerNameHelper
- def current_controller_name(controller)
-   #params[:controller].capitalize
-   controller.controller_name.humanize
+ def current_controller_name
+   controller_name.humanize
  end
 end
--- test/helpers/controller_name_helper_test.rb
+++ test/helpers/controller_name_helper_test.rb
@@ -1,8 +1,8 @@
 require 'test_helper'
 
 class ControllerNameHelperTest < ActionView::TestCase
- test 'should return ble ble' do
-  assert_equal "Application", current_controller_name(ApplicationController.new())
-  assert_not_equal "Test", current_controller_name(ParkingsController.new())
+ test 'should return name of current controller in human readable form' do
+  assert_equal "Test", current_controller_name
+  assert_not_equal "Application", current_controller_name
  end
 end
