650f5cf5da2ddfae7401dd61d875f5a749a3b56e
Magda Gumularz <mmgumularz@gmail.com>
2014-11-11 20:27:39 +0100

    #48 passing integration test for place rents, also small change in place rent form

--- test/integration/place_rents_test.rb
+++ test/integration/place_rents_test.rb
@@ -5,12 +5,9 @@ class PlaceRentsTest < ActionDispatch::IntegrationTest
  test "user creates new place rent" do
   visit parkings_path
   click_link 'rent place', href: new_parking_place_rent_path(Parking.first)
-  #save_and_open_page
-
   assert_equal current_url, new_parking_place_rent_url(Parking.first)
   select("yaris", from: "place_rent_car_id")
   click_button('go')
-  #save_and_open_page
   assert_equal current_url, place_rent_url(PlaceRent.last)
   assert has_content? "You've rented the place"
   assert has_content? "Car: model - yaris"
