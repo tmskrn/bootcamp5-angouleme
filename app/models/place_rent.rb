256096871d789ec207cf41526eef25de90426d3d
Magda Gumularz <mmgumularz@gmail.com>
2014-11-12 14:34:59 +0100

    #50 calculating price in place rents plus test for it

--- app/models/place_rent.rb
+++ app/models/place_rent.rb
@@ -1,6 +1,26 @@
 class PlaceRent < ActiveRecord::Base
   belongs_to :car
   belongs_to :parking
+  before_save :calc_price
 
   validates :start_date, :end_date, :parking, :car, presence: true
+
+  def finish_rent
+    self.end_date = Time.now if end_date > Time.now
+  end
+
+  def calc_price
+    #self.price = 2
+    self.price = days_rate + hours_rate   
+  end
+
+  private
+  def days_rate
+    ((end_date - start_date).to_i / 1.day ) * parking.day_price.to_f 
+  end
+
+  def hours_rate
+    ((end_date - start_date).to_i % 1.day / 1.hour).to_i * parking.hour_price.to_f
+  end
+
 end
--- test/models/place_rent_test.rb
+++ test/models/place_rent_test.rb
@@ -2,26 +2,34 @@ require 'test_helper'
 
 class PlaceRentTest < ActiveSupport::TestCase
   test "should not save place rent without start date" do
-   place_rent = PlaceRent.new(end_date: Time.now + 3.days, parking: parkings(:wroclaw), car: cars(:porsche))
-   assert place_rent.invalid?
-   assert_not place_rent.errors[:start_date].empty?
+    place_rent = PlaceRent.new(end_date: Time.now + 3.days, parking: parkings(:wroclaw), car: cars(:porsche))
+    assert place_rent.invalid?
+    assert_not place_rent.errors[:start_date].empty?
   end
 
   test "should not save place rent without end date" do
-   place_rent = PlaceRent.new(start_date: Time.now, parking: parkings(:wroclaw), car: cars(:porsche))
-   assert place_rent.invalid?
-   assert_not place_rent.errors[:end_date].empty?
+    place_rent = PlaceRent.new(start_date: Time.now, parking: parkings(:wroclaw), car: cars(:porsche))
+    assert place_rent.invalid?
+    assert_not place_rent.errors[:end_date].empty?
   end
 
   test "should not save place rent without parking" do
-   place_rent = PlaceRent.new(start_date: Time.now, end_date: Time.now + 2.days, car: cars(:porsche))
-   assert place_rent.invalid?
-   assert_not place_rent.errors[:parking].empty?
+    place_rent = PlaceRent.new(start_date: Time.now, end_date: Time.now + 2.days, car: cars(:porsche))
+    assert place_rent.invalid?
+    assert_not place_rent.errors[:parking].empty?
   end
 
   test "should not save place rent without car" do
-   place_rent = PlaceRent.new(start_date: Time.now, end_date: Time.now + 2.days,  parking: parkings(:wroclaw))
-   assert place_rent.invalid?
-   assert_not place_rent.errors[:car].empty?
+    place_rent = PlaceRent.new(start_date: Time.now, end_date: Time.now + 2.days,  parking: parkings(:wroclaw))
+    assert place_rent.invalid?
+    assert_not place_rent.errors[:car].empty?
+  end
+
+  test "should calculate total price when created" do
+    place_rent = PlaceRent.create(start_date: Time.now, end_date: Time.now.advance(:days => 2, :hours => 4),  parking: parkings(:wroclaw), car: cars(:porsche))
+
+    assert place_rent.valid?
+    #assert_equal "Wroclaw", place_rent.parking.address.city
+    assert_equal 55.4, place_rent.price.to_f   
   end
 end
