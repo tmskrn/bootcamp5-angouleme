52aa0737075c604d62bbb1839e9854c7425e8126
Magda Gumularz <mmgumularz@gmail.com>
2014-11-13 15:49:31 +0100

    #58 test for full_name method

--- test/models/person_test.rb
+++ test/models/person_test.rb
@@ -6,4 +6,12 @@ class PersonTest < ActiveSupport::TestCase
     assert person.invalid?
     assert_not person.errors[:first_name].empty? 
   end
+
+  test "should return person's  full name" do
+    person = Person.new(first_name: "Jimmy", last_name: "Kimmel")
+    assert_equal "Jimmy Kimmel", person.full_name
+
+    person2 = Person.new(first_name: "Bobby")
+    assert_equal "Bobby", person2.full_name
+  end
 end
