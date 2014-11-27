747e9cfc3e32c5c2aca781ca4b692491533b2a75
Magda Gumularz <mmgumularz@gmail.com>
2014-11-27 19:48:58 +0100

    #61 syntax fix

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,12 +5,13 @@ class SessionsController < ApplicationController
 
   def create
     account = Account.authenticate(params[:email], params[:password])
-    if account.nil?
-      flash.now.alert = 'Invalid email or password. Please try again.'
-      render 'new'
-    else  
+
+    if account
       session[:current_account_id] = account.id
       redirect_to session[:return_to].nil? ? root_url : session[:return_to], notice: 'You have successfully logged in.'
+    else
+      flash.now.alert = 'Invalid email or password. Please try again.'
+      render 'new'  
     end
   end
 
